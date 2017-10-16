93346f58a57b72c78f0c1aa5e92d4fbf365ddd0b
johndvnguyen <johndvnguyen@gmail.com>
2017-10-16 16:57:36 -0700

    Update measureTask.h

--- measureTask.h
+++ measureTask.h
@@ -6,5 +6,9 @@ void measureTemp(void* measureData);
 void measureSysBP(void* measureData);
 void measureDiaBP(void* measureData);
 void measurePR(void* measureData);
+void measureTempArray(void* data);
+void measureSysBPArray(void* data);
+void measureDiaBPArray(void* data);
+void measurePRArray(void* data);
 
 #endif
