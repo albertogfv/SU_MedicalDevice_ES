566d8756723c0c96d6610f7f7486a3c472b88232
John Nguyen <johndvnguyen@gmail.com>
2017-10-20 01:43:11 -0700

    Moved the button test to its own file and function
    It is a test function with an infinite loop but its detecting user input at least

---
+++ buttontest.h
@@ -0,0 +1,20 @@
+#ifndef BUTTONTEST_H__
+#define BUTTONTEST_H__
+
+//*****************************************************************************
+//
+// The vertical counter used to debounce the push buttons.  The bit positions
+// are the same as g_ucSwitches.
+//
+//*****************************************************************************
+static unsigned char g_ucSwitchClockA;
+static unsigned char g_ucSwitchClockB;
+
+//*****************************************************************************
+//
+// Handles the SysTick timeout interrupt.
+//
+//*****************************************************************************
+void SysTickIntHandler(void);
+
+#endif
